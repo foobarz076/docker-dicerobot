fb21ce7a0f78117d25afc8ce69a28b846fdcb75e
Foobarz076 <foobarz076@gmail.com>
2022-10-05 19:06:07 +0800

    Create for docker-compose

--- dockerfiles/latest/init.sh
+++ dockerfiles/latest/init.sh
@@ -20,13 +20,11 @@ printf "\033[32m1. 输入 QQ 账号信息\033[0m\n"
 
 while true
 do
-  read -r -p "请输入机器人的 QQ 号码: " qq_id
-  read -r -p "请输入机器人的 QQ 密码: " qq_password
 
   printf "\n****************************************\n"
   printf "%-15s   %-20s\n" " QQ 号码" "   QQ 密码"
   printf "****************************************\n"
-  printf "%-15s   %-20s\n" "${qq_id}" "${qq_password}"
+  printf "%-15s   %-20s\n" "${MIRAI_USER_ID}" "${MIRAI_USER_PASSWORD}"
   printf "****************************************\n"
   printf "\033[33m请确认以上信息是否正确？\033[0m [Y/N] "
   read -r is_correct
@@ -64,22 +62,22 @@ printf "\nDone\n\n"
 # Deploy Mirai
 printf "\033[32m3. 部署 Mirai\033[0m\n"
 
-wget -qO mirai.zip https://dl.drsanwujiang.com/dicerobot/mirai/mirai-mcl-2.3.1.zip
+wget -O /tmp/mirai.zip https://dl.drsanwujiang.com/dicerobot/mirai/mirai-mcl-2.3.1.zip
 
 if [ $? -ne 0 ]; then
   process_failed "下载 Mirai 失败"
 fi
 
-unzip -qq -o mirai.zip -d mirai
-rm -f mirai.zip
+unzip -o /tmp/mirai.zip -d /mirai
+rm -f /tmp/mirai.zip
 
 printf "\nDone\n\n"
 
 # Deploy DiceRobot
 printf "\033[32m3. 部署 DiceRobot\033[0m\n"
 
-if [ "$(ls -A dicerobot)" = "" ]; then
-  composer --no-interaction --quiet create-project drsanwujiang/dicerobot-skeleton:3.1.0 dicerobot --no-dev
+if [ "$(ls -A /dicerobot)" = "" ]; then
+  composer --no-interaction create-project drsanwujiang/dicerobot-skeleton:3.1.0 /dicerobot --no-dev
 
   if [ $? -ne 0 ]; then
     process_failed "部署 DiceRobot 失败"
@@ -87,14 +85,14 @@ if [ "$(ls -A dicerobot)" = "" ]; then
 else
   printf "\033[33m检测到 DiceRobot 目录不为空，更新 DiceRobot……\033[0m\n"
 
-  wget -qO dicerobot-update.zip https://dl.drsanwujiang.com/dicerobot/skeleton-update/skeleton-update-3.1.0.zip
+  wget -O /tmp/dicerobot-update.zip https://dl.drsanwujiang.com/dicerobot/skeleton-update/skeleton-update-3.1.0.zip
 
   if [ $? -ne 0 ]; then
     process_failed "下载 DiceRobot 更新包失败"
   fi
 
-  unzip -qq -o dicerobot-update.zip -d dicerobot
-  composer --no-interaction --quiet update --working-dir dicerobot --no-dev
+  unzip -o /tmp/dicerobot-update.zip -d /dicerobot
+  composer --no-interaction update --working-dir /dicerobot --no-dev
 
   if [ $? -ne 0 ]; then
     process_failed "更新 DiceRobot 失败"
@@ -106,20 +104,63 @@ printf "\nDone\n\n"
 # Initialization
 printf "\033[32m4. 初始化\033[0m\n"
 
-sed -i "0,/10000/{s/10000/${qq_id}/}" dicerobot/config/custom_config.php
+cat > /dicerobot/config/custom_config.php <<EOF
+<?php
 
-cat > mirai/config/Console/AutoLogin.yml <<EOF
+use Monolog\Logger;
+
+/**
+ * 自定义配置
+ */
+
+/******************************************************************************
+ *                                    必填项                                   *
+ ******************************************************************************/
+
+/**
+ * 在这里填写机器人的 QQ 号，以及 Mirai API HTTP 插件中的 Auth Key
+ */
+$settings["mirai"]["robot"] = [
+    "id" => ${MIRAI_USER_ID},
+    "authKey" => "12345678"
+];
+
+/******************************************************************************
+ *                                    常用项                                   *
+ ******************************************************************************/
+
+/**
+ * 在这里填写 Mirai API HTTP 插件监听的端口
+ */
+$settings["mirai"]["server"]["host"] = "mirai";
+$settings["mirai"]["server"]["port"] = 8080;
+
+/**
+ * 在这里填写 DiceRobot 监听的端口
+ */
+$settings["dicerobot"]["server"]["port"] = 9500;
+
+/**
+ * 在这里设置日志等级，file 表示日志文件的等级，console 表示控制台日志的等级
+ */
+$settings["log"]["level"] = [
+    "file" => Logger::NOTICE,
+    "console" => Logger::NOTICE
+];
+EOF
+
+cat > /mirai/config/Console/AutoLogin.yml <<EOF
 accounts:
   -
-    account: ${qq_id}
+    account: ${MIRAI_USER_ID}
     password:
       kind: PLAIN
-      value: ${qq_password}
+      value: ${MIRAI_USER_PASSWORD}
     configuration:
       protocol: ANDROID_PHONE
 EOF
 
-cat > mirai/config/net.mamoe.mirai-api-http/setting.yml <<EOF
+cat > /mirai/config/net.mamoe.mirai-api-http/setting.yml <<EOF
 adapters:
   - http
   - webhook
@@ -133,57 +174,15 @@ cacheSize: 4096
 
 adapterSettings:
   http:
-    host: 127.0.0.1
+    host: 0.0.0.0
     port: 8080
     cors:
       - *
 
   webhook:
     destinations:
-      - "http://127.0.0.1:9500/report"
+      - "http://dicerobot:9500/report"
 EOF
-
-cat > /etc/systemd/system/dicerobot.service <<EOF
-[Unit]
-Description=A TRPG dice robot based on Swoole
-After=network.target
-After=syslog.target
-Before=mirai.service
-
-[Service]
-User=$(id -un)
-Group=$(id -gn)
-Type=simple
-ExecStart=/usr/local/bin/php /root/dicerobot/dicerobot.php
-ExecReload=/bin/kill -12 \$MAINPID
-RestartSec=1s
-RestartForceExitStatus=99
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-cat > /etc/systemd/system/mirai.service <<EOF
-[Unit]
-Description=Mirai Console
-After=network.target
-After=syslog.target
-After=dicerobot.service
-
-[Service]
-Type=simple
-Environment="PATH=/opt/java/openjdk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-WorkingDirectory=/root/mirai
-ExecStart=/bin/bash /root/mirai/start-mirai.sh
-ExecStop=/bin/bash /root/mirai/stop-mirai.sh
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-systemctl daemon-reload > /dev/null 2>&1
-systemctl enable dicerobot > /dev/null 2>&1
-systemctl enable mirai > /dev/null 2>&1
 
 printf "\nDone\n\n"
 
